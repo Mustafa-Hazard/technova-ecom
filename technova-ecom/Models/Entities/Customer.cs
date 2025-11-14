67f194596f8bcddbb668222adc8a69971ef33342
2312292 <2312292@PC31>
2025-11-14 12:16:05 +0500

    Update customer table and navigation links
    Updated `Index.cshtml` to replace the `Password` column with `Address` and add a `PhoneNumber` column. Adjusted the table rows to display the new fields. Changed the title to "List of customer" and added a "Create New" link.
    
    Modified `_Layout.cshtml` to update the "Customers" navigation link to use `asp-controller="Customers"` and `asp-action="Index"`.

--- technova-ecom/Models/Entities/Customer.cs
+++ technova-ecom/Models/Entities/Customer.cs
@@ -17,21 +17,30 @@ namespace Models.Entities
         public int CustomerId { get; set; }
 
         [Column("first_name")]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Column("last_name")]
+        [Display(Name = "Surname")]
         public string LastName { get; set; }
 
         [Column("email")]
+        [Display(Name = "email")]
+        //Validation for email format
+        //regex pattern for email validation
+
         public string Email { get; set; }
 
         [Column("password")]
+        
         public string Password { get; set; }
 
         [Column("address")]
+        [Display(Name = "address")]
         public string Address { get; set; }
 
         [Column("phone_number")]
+        [Display(Name = "phone number")]
         public string PhoneNumber { get; set; }
     }
 }
